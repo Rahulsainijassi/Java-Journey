cfea34a1adab8de92aacc1caf7699378bfd7a123
Rahul saini <134114955+Rahulsainijassi@users.noreply.github.com>
2024-02-14 21:22:39 +0530

    Add files via upload

---
+++ impmypack.java
@@ -0,0 +1,10 @@
+package newpackjava;
+import mypack.*;
+
+public class impmypack extends packagejava {
+    public static void main(String args[]) {
+        impmypack obj = new impmypack();
+        obj.show();
+    }
+
+}
---
+++ packagejava.java
@@ -0,0 +1,7 @@
+package mypack;
+
+public class packagejava {
+    protected void show() {
+        System.out.println("yes its working");
+    }
+}
